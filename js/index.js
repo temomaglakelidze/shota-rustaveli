19f4bc14be405f7da16ad2ded1c503a3d673c70e
temomaglakelidze <154346509+temomaglakelidze@users.noreply.github.com>
2024-06-07 13:31:40 +0400

    header and hero

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 const cursor = document.querySelector(".cursor");
+const listItems = document.querySelectorAll(".list-item");
+const element = document.querySelector(".cursor");
+const hero = document.querySelector(".hero");
+
 document.addEventListener("mousemove", (e) => {
   cursor.style.left = e.pageX + "px";
   cursor.style.top = e.pageY + "px";
+});
+
+listItems.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    element.classList.add("scaled");
+    item.classList.add("scaled-item");
+  });
+
+  item.addEventListener("mouseout", () => {
+    element.classList.remove("scaled");
+    item.classList.remove("scaled-item");
+  });
+});
+
+hero.addEventListener("mouseover", () => {
+  cursor.classList.add("hero-cursor");
+});
+
+hero.addEventListener("mouseout", () => {
+  cursor.classList.remove("hero-cursor");
 });
