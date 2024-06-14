a3e8d96a8c9db2dc23c923411d550beee1e43343
temomaglakelidze <154346509+temomaglakelidze@users.noreply.github.com>
2024-06-14 20:58:56 +0400

    making main section

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ navBar.addEventListener("mouseover", () => {
 navBar.addEventListener("mouseout", () => {
   cursor.classList.remove("navbar-cursor");
 });
+
 navBarLink.forEach((navBarLink) => {
   navBarLink.addEventListener("mouseover", () => {
     cursor.classList.add("link-cursor");
@@ -28,4 +29,37 @@ searchContent.addEventListener("mouseover", () => {
 });
 searchContent.addEventListener("mouseout", () => {
   cursor.classList.remove("search-cursor");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBar = document.querySelector(".hidden-navbar");
+  const menuToggleBtn = document.querySelector(".hidden-navbar-arrow");
+  const overlay = document.querySelector(".overlay");
+
+  document.addEventListener("mousemove", (e) => {
+    if (window.innerWidth < 1200 && e.clientX <= window.innerWidth * 0.1) {
+      menuToggleBtn.classList.add("visible");
+    } else if (!menuBar.classList.contains("visible")) {
+      menuToggleBtn.classList.remove("visible");
+    }
+  });
+
+  menuToggleBtn.addEventListener("click", () => {
+    menuToggleBtn.style.transform = "rotate(180deg)";
+    const isMenuVisible = menuBar.classList.toggle("visible");
+    if (isMenuVisible) {
+      overlay.classList.toggle("visible");
+      menuToggleBtn.classList.add("visible");
+    } else {
+      overlay.classList.remove("visible");
+      menuToggleBtn.style.transform = "rotate(0deg)";
+    }
+  });
+
+  overlay.addEventListener("click", () => {
+    menuToggleBtn.style.transform = "rotate(0deg)";
+    menuToggleBtn.classList.remove("visible");
+    menuBar.classList.remove("visible");
+    overlay.classList.remove("visible");
+  });
 });
